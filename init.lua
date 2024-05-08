dff5c263f034f670bbafb39bf3c0572823e9d6bd
quim@turin <quimortiz21@gmail.com>
2024-05-08 10:04:14 -0400

    minor dev

--- init.lua
+++ init.lua
@@ -778,7 +778,11 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      require('tokyonight').setup {
+        terminal_colors = true,
+        sidebars = { 'qf', 'vista_kind', 'terminal', 'packer', 'term' },
+      }
+      vim.cmd.colorscheme 'tokyonight-day'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -876,12 +880,12 @@ require('lazy').setup({
   require 'custom.plugins.vimtex',
   require 'custom.plugins.nvim-surround',
   require 'custom.plugins.NotebookNavigator',
-  require 'custom.plugins.catppuccin',
   require 'custom.plugins.asyncrun',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
   -- require 'custom.plugins.toggleterm',
   -- require 'custom.plugins.bg',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  -- require 'custom.plugins.catppuccin',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
