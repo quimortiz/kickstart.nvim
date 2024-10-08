dee528b650478b09dd5674c474503e7a82742b5c
quim@mango <quimortiz21@gmail.com>
2024-10-08 15:40:13 -0400

    minor keymap

--- init.lua
+++ init.lua
@@ -553,6 +553,12 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          -- **New Keybinding: Go to Definition in Vertical Split**
+          map('<leader>gd', function()
+            vim.cmd.vsplit()
+            vim.lsp.buf.definition()
+          end, 'Go to Definition in Vertical Split')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
